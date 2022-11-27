74b79acf88707a047b5930c0c2d9fee3088a8a64
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2022-11-27 18:01:11 +0100

    cleaned up the start code

--- Tetris-Algorithm/main.cpp
+++ Tetris-Algorithm/main.cpp
@@ -10,6 +10,49 @@
 #undef max
 #endif
 
+void InitializeTetris();
+void GetDesktopResolution(LONG& x, LONG& y);
+void ConvertNDCToScreenCoords(LONG& x, LONG& y, const Point& wantedCoords, const uint32_t screenX, const uint32_t screenY);
+void SendMousePress(const Point& coords);
+
+inline constexpr Point g_LeftCoords{ 84L, 252L };
+inline constexpr Point g_DownCoords{ 111L, 306L };
+inline constexpr Point g_UpCoords{ 111L, 239L };
+inline constexpr Point g_RightCoords{ 138L, 252L };
+
+inline constexpr const char g_TetrisWindow[13]{ "NES - Tetris" };
+inline constexpr const char g_VirtualPadsWindow[13]{ "Virtual Pads" };
+
+inline LONG g_ScreenWidth{};
+inline LONG g_ScreenHeight{};
+
+int main()
+{
+	/* NOTE: This does NOT start the emulator, or load the Tetris ROM, those must be started by the user! */
+	/* Emulator download link: https://www.emulator-zone.com/misc/bizhawk */
+	/* Rom download link: https://www.emulatorgames.net/roms/nintendo/tetris/ */
+	InitializeTetris();
+
+	Sleep(1000);
+
+	return 0;
+}
+
+void InitializeTetris()
+{
+	SetProcessDPIAware();
+	GetDesktopResolution(g_ScreenWidth, g_ScreenHeight);
+
+	HWND tetris{ FindWindowA(NULL, g_TetrisWindow) };
+	__ASSERT(tetris != nullptr);
+
+	HWND tetrisVirtualPad{ FindWindowA(NULL, g_VirtualPadsWindow) };
+	__ASSERT(tetrisVirtualPad != nullptr);
+
+	SetForegroundWindow(tetris);
+	SetForegroundWindow(tetrisVirtualPad);
+}
+
 void GetDesktopResolution(LONG& x, LONG& y)
 {
 	RECT desktop{};
@@ -33,40 +76,13 @@ void ConvertNDCToScreenCoords(LONG& x, LONG& y, const Point& wantedCoords, const
 	y = static_cast<LONG>(std::numeric_limits<uint16_t>::max() / (float)screenY * (float)wantedCoords.y);
 }
 
-inline constexpr Point g_LeftCoords{ 84L, 252L };
-inline constexpr Point g_DownCoords{ 111L, 306L };
-inline constexpr Point g_UpCoords{ 111L, 239L };
-inline constexpr Point g_RightCoords{ 138L, 252L };
-
-inline constexpr const char g_TetrisWindow[13]{ "NES - Tetris" };
-inline constexpr const char g_VirtualPadsWindow[13]{ "Virtual Pads" };
-
-int main()
+void SendMousePress(const Point& coords)
 {
-	SetProcessDPIAware();
-
-	LONG x{};
-	LONG y{};
-	GetDesktopResolution(x, y);
-
-	HWND tetris{ FindWindowA(NULL, g_TetrisWindow) };
-	__ASSERT(tetris != nullptr);
-
-	HWND tetrisVirtualPad{ FindWindowA(NULL, g_VirtualPadsWindow) };
-	__ASSERT(tetrisVirtualPad != nullptr);
-
-	SetForegroundWindow(tetris);
-	Sleep(1000);
-	SetForegroundWindow(tetrisVirtualPad);
-	Sleep(1000);
-
 	INPUT input{};
 
 	input.type = INPUT_MOUSE;
-	ConvertNDCToScreenCoords(input.mi.dx, input.mi.dy, g_LeftCoords, x, y);
+	ConvertNDCToScreenCoords(input.mi.dx, input.mi.dy, coords, g_ScreenWidth, g_ScreenHeight);
 	input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP;
 
 	SendInput(1, &input, sizeof(INPUT));
-
-	return 0;
 }
