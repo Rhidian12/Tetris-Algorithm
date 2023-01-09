aac547330f00432e5f58a522a918adc3c46d9458
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2023-01-09 22:40:59 +0100

    cleaning up

--- Tetris-Algorithm/Board/Board.cpp
+++ Tetris-Algorithm/Board/Board.cpp
@@ -53,86 +53,86 @@ void Board::Add(const std::array<Tetris::Point, g_MaxNrOfBlocks>& points)
 		m_BoardState[p.y][p.x] = true;
 }
 
-//BOOL SaveHBITMAPToFile(HBITMAP hBitmap, LPCTSTR lpszFileName)
-//{
-//	HDC hDC;
-//	int iBits;
-//	WORD wBitCount;
-//	DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
-//	BITMAP Bitmap0;
-//	BITMAPFILEHEADER bmfHdr;
-//	BITMAPINFOHEADER bi;
-//	LPBITMAPINFOHEADER lpbi;
-//	HANDLE fh, hDib, hPal, hOldPal2 = NULL;
-//	hDC = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
-//	iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES);
-//	DeleteDC(hDC);
-//	if (iBits <= 1)
-//		wBitCount = 1;
-//	else if (iBits <= 4)
-//		wBitCount = 4;
-//	else if (iBits <= 8)
-//		wBitCount = 8;
-//	else
-//		wBitCount = 24;
-//	GetObject(hBitmap, sizeof(Bitmap0), (LPSTR)&Bitmap0);
-//	bi.biSize = sizeof(BITMAPINFOHEADER);
-//	bi.biWidth = Bitmap0.bmWidth;
-//	bi.biHeight = -Bitmap0.bmHeight;
-//	bi.biPlanes = 1;
-//	bi.biBitCount = wBitCount;
-//	bi.biCompression = BI_RGB;
-//	bi.biSizeImage = 0;
-//	bi.biXPelsPerMeter = 0;
-//	bi.biYPelsPerMeter = 0;
-//	bi.biClrImportant = 0;
-//	bi.biClrUsed = 256;
-//	dwBmBitsSize = ((Bitmap0.bmWidth * wBitCount + 31) & ~31) / 8
-//		* Bitmap0.bmHeight;
-//	hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
-//	lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
-//	*lpbi = bi;
-//
-//	hPal = GetStockObject(DEFAULT_PALETTE);
-//	if (hPal)
-//	{
-//		hDC = GetDC(NULL);
-//		hOldPal2 = SelectPalette(hDC, (HPALETTE)hPal, FALSE);
-//		RealizePalette(hDC);
-//	}
-//
-//
-//	GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap0.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
-//		+ dwPaletteSize, (BITMAPINFO*)lpbi, DIB_RGB_COLORS);
-//
-//	if (hOldPal2)
-//	{
-//		SelectPalette(hDC, (HPALETTE)hOldPal2, TRUE);
-//		RealizePalette(hDC);
-//		ReleaseDC(NULL, hDC);
-//	}
-//
-//	fh = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
-//		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
-//
-//	if (fh == INVALID_HANDLE_VALUE)
-//		return FALSE;
-//
-//	bmfHdr.bfType = 0x4D42; // "BM"
-//	dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
-//	bmfHdr.bfSize = dwDIBSize;
-//	bmfHdr.bfReserved1 = 0;
-//	bmfHdr.bfReserved2 = 0;
-//	bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
-//
-//	WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
-//
-//	WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL);
-//	GlobalUnlock(hDib);
-//	GlobalFree(hDib);
-//	CloseHandle(fh);
-//	return TRUE;
-//}
+BOOL SaveHBITMAPToFile(HBITMAP hBitmap, LPCTSTR lpszFileName)
+{
+	HDC hDC;
+	int iBits;
+	WORD wBitCount;
+	DWORD dwPaletteSize = 0, dwBmBitsSize = 0, dwDIBSize = 0, dwWritten = 0;
+	BITMAP Bitmap0;
+	BITMAPFILEHEADER bmfHdr;
+	BITMAPINFOHEADER bi;
+	LPBITMAPINFOHEADER lpbi;
+	HANDLE fh, hDib, hPal, hOldPal2 = NULL;
+	hDC = CreateDC(TEXT("DISPLAY"), NULL, NULL, NULL);
+	iBits = GetDeviceCaps(hDC, BITSPIXEL) * GetDeviceCaps(hDC, PLANES);
+	DeleteDC(hDC);
+	if (iBits <= 1)
+		wBitCount = 1;
+	else if (iBits <= 4)
+		wBitCount = 4;
+	else if (iBits <= 8)
+		wBitCount = 8;
+	else
+		wBitCount = 24;
+	GetObject(hBitmap, sizeof(Bitmap0), (LPSTR)&Bitmap0);
+	bi.biSize = sizeof(BITMAPINFOHEADER);
+	bi.biWidth = Bitmap0.bmWidth;
+	bi.biHeight = -Bitmap0.bmHeight;
+	bi.biPlanes = 1;
+	bi.biBitCount = wBitCount;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrImportant = 0;
+	bi.biClrUsed = 256;
+	dwBmBitsSize = ((Bitmap0.bmWidth * wBitCount + 31) & ~31) / 8
+		* Bitmap0.bmHeight;
+	hDib = GlobalAlloc(GHND, dwBmBitsSize + dwPaletteSize + sizeof(BITMAPINFOHEADER));
+	lpbi = (LPBITMAPINFOHEADER)GlobalLock(hDib);
+	*lpbi = bi;
+
+	hPal = GetStockObject(DEFAULT_PALETTE);
+	if (hPal)
+	{
+		hDC = GetDC(NULL);
+		hOldPal2 = SelectPalette(hDC, (HPALETTE)hPal, FALSE);
+		RealizePalette(hDC);
+	}
+
+
+	GetDIBits(hDC, hBitmap, 0, (UINT)Bitmap0.bmHeight, (LPSTR)lpbi + sizeof(BITMAPINFOHEADER)
+		+ dwPaletteSize, (BITMAPINFO*)lpbi, DIB_RGB_COLORS);
+
+	if (hOldPal2)
+	{
+		SelectPalette(hDC, (HPALETTE)hOldPal2, TRUE);
+		RealizePalette(hDC);
+		ReleaseDC(NULL, hDC);
+	}
+
+	fh = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+
+	if (fh == INVALID_HANDLE_VALUE)
+		return FALSE;
+
+	bmfHdr.bfType = 0x4D42; // "BM"
+	dwDIBSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + dwPaletteSize + dwBmBitsSize;
+	bmfHdr.bfSize = dwDIBSize;
+	bmfHdr.bfReserved1 = 0;
+	bmfHdr.bfReserved2 = 0;
+	bmfHdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER) + dwPaletteSize;
+
+	WriteFile(fh, (LPSTR)&bmfHdr, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
+
+	WriteFile(fh, (LPSTR)lpbi, dwDIBSize, &dwWritten, NULL);
+	GlobalUnlock(hDib);
+	GlobalFree(hDib);
+	CloseHandle(fh);
+	return TRUE;
+}
 
 void Board::SetBoardState()
 {
@@ -140,15 +140,13 @@ void Board::SetBoardState()
 		for (int c{}; c < m_BoardSize.x; ++c)
 			m_BoardState[r][c] = false;
 
-	// auto tempBoardState{ m_BoardState };
-
 	// get screen dimensions
-	int x1 = GetSystemMetrics(SM_XVIRTUALSCREEN);
-	int y1 = GetSystemMetrics(SM_YVIRTUALSCREEN);
-	int x2 = GetSystemMetrics(SM_CXVIRTUALSCREEN);
-	int y2 = GetSystemMetrics(SM_CYVIRTUALSCREEN);
-	int w = x2 - x1;
-	int h = y2 - y1;
+	const int x1{ GetSystemMetrics(SM_XVIRTUALSCREEN) };
+	const int y1{ GetSystemMetrics(SM_YVIRTUALSCREEN) };
+	const int x2{ GetSystemMetrics(SM_CXVIRTUALSCREEN) };
+	const int y2{ GetSystemMetrics(SM_CYVIRTUALSCREEN) };
+	const int w{ x2 - x1 };
+	const int h{ y2 - y1 };
 
 	// copy screen to bitmap
 	HDC     hScreen = GetDC(NULL); // get the handle to the device context for the entire screen 
@@ -179,34 +177,6 @@ void Board::SetBoardState()
 	DeleteDC(hDC);
 	ReleaseDC(NULL, hScreen);
 	DeleteObject(hBitmap);
-
-#pragma region STB
-	/* Analyze the image with STB */
-	//int width{};
-	//int height{};
-	//int bytesPerPixel{};
-	//unsigned char* pData = stbi_load(m_pScreenGrabber->GetPathToScreenshot().data(), &width, &height, &bytesPerPixel, 3);
-
-	//for (int r{}; r < m_BoardSize.y - 2; ++r)
-	//{
-	//	for (int c{}; c < m_BoardSize.x; ++c)
-	//	{
-	//		const int x{ ((c * (m_BlockSize.x + m_BlockOffset.x)) + m_ScreenStart.x) };
-	//		const int y{ (m_ScreenStart.y - (r * (m_BlockSize.y + m_BlockOffset.y))) };
-
-	//		const unsigned char* pPixel{ pData + (x + width * y) * bytesPerPixel };
-	//		const unsigned char red = pPixel[0];
-	//		const unsigned char green = pPixel[1];
-	//		const unsigned char blue = pPixel[2];
-
-	//		/* If the pixel is not black we want to save it */
-	//		if (red != 0 || green != 0 || blue != 0)
-	//			tempBoardState[r][c] = true;
-	//	}
-	//}
-
-	//stbi_image_free(pData);
-#pragma endregion
 
 	if (DoesRowContainPieces(m_BoardSize.y - 3) || DoesRowContainPieces(m_BoardSize.y - 4))
 		m_OnNewPieceSpawned.Invoke();
